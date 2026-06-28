27961ddb3ba6ad82ae7778068a08c080f133e609
Piyush Nandre <nandrepiyush04@gmail.com>
2026-06-28 16:33:17 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.express as px
+
 import plotly.graph_objects as go
 
 from pypfopt.expected_returns import mean_historical_return
